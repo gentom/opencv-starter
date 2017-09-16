f01056c56cd663c10a9dafa67ccd4b6f4a9f44bb
gentom <gento.m.developer@gmail.com>
2017-09-16 23:11:28 +0900

    commit

---
+++ opencv_starter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_starter
+//
+//  Created by Morikawa Gento on 2017/09/16.
+//  Copyright © 2017年 gentom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
