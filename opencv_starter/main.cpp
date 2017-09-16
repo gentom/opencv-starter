bdd3fdfe02ad849d2958f3c8816fc03d64ad3ac8
gentom <gento.m.developer@gmail.com>
2017-09-16 23:50:08 +0900

    commit

--- opencv_starter/main.cpp
+++ opencv_starter/main.cpp
@@ -11,9 +11,24 @@
 
 int main(int argc, const char * argv[])
 {
+    /*
     cv::Mat img(cv::Size(320, 240), CV_8UC3, cv::Scalar(60, 150, 80));
     cv::namedWindow("OpenCV3!", cv::WINDOW_AUTOSIZE);
     cv::imshow("OpenCV3!", img);
     
     cv::waitKey(0);
+    */
+    
+    //赤い画像を用意
+    cv::Mat red(cv::Size(320, 240), CV_8UC3, cv::Scalar(0, 0, 255));
+    //Redウィンドウを作成
+    cv::namedWindow("Red", cv::WINDOW_AUTOSIZE);
+    //Redウィンドウに赤画像を表示
+    cv::imshow("Red", red);
+    //ユーザの入力を待つ
+    cv::waitKey(0);
+    //全てのウィンドウを閉じる
+    cv::destroyAllWindows();
+    
+    return 0;
 }
